2dfb36b25c25a8681e7e6fa0051255539c7f2c19
fordgoat <adaadaajapan@gmail.com>
2024-06-04 03:46:30 +0700

    Update __init__.py

--- plugins/command/__init__.py
+++ plugins/command/__init__.py
@@ -3,10 +3,4 @@ from .tambah_admin import *
 from .send_handler import * 
 from .ban_handler import *
 from .bot_handler import *
-from .talent_handler import *
-from .daddy_sugar import *
-from .moans_girl import *
-from .moans_boy import *
-from .boyfriend_rent import *
-from .girlfriend_rent import *
 from .broadcast_handler import *
