8280fa8006352c023ca8a00e394fc265d234e9cb
fordgoat <adaadaajapan@gmail.com>
2024-06-04 04:06:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,24 +21,12 @@ id_admin = int(os.getenv("ID_ADMIN", "1707380693"))
 # =========================================================== #
 
 batas_kirim = int(os.getenv("BATAS_KIRIM", "5"))
-batas_talent = int(os.getenv("BATAS_TALENT", "10"))
-batas_daddy_sugar = int(os.getenv("BATAS_DADDY_SUGAR", "10"))
-batas_moansgirl = int(os.getenv("BATAS_MOANSGIRL", "10"))
-batas_moansboy = int(os.getenv("BATAS_MOANSBOY", "10"))
-batas_gfrent = int(os.getenv("BATAS_GFRENT", "10"))
-batas_bfrent = int(os.getenv("BATAS_BFRENT", "10"))
 # =========================================================== #
 
-biaya_kirim = int(os.getenv("BIAYA_KIRIM", "10"))
-biaya_talent = int(os.getenv("BIAYA_TALENT", "80"))
-biaya_daddy_sugar = int(os.getenv("BIAYA_DADDY_SUGAR", "70"))
-biaya_moansgirl = int(os.getenv("BIAYA_MOANSGIRL", "60"))
-biaya_moansboy = int(os.getenv("BIAYA_MOANSBOY", "50"))
-biaya_gfrent = int(os.getenv("BIAYA_GFRENT", "40"))
-biaya_bfrent = int(os.getenv("BIAYA_BFRENT", "30"))
+biaya_kirim = int(os.getenv("BIAYA_KIRIM", "20"))
 # =========================================================== #
 
-hastag = os.getenv("HASTAG", "#Girl #Boy #Ask #Find #Spill #Story").replace(" ", "|").lower()
+hastag = os.getenv("HASTAG", "#s #a #FwbBoy #FwbGirl #FwbAsk #FwbStory").replace(" ", "|").lower()
 # =========================================================== #
 
 pic_boy = os.getenv("PIC_BOY", "https://telegra.ph/file/c67bd36023648dc777bd9.jpg")
