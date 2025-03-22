0dee3d4acb446f2938ad0bcd0a5aec0e3092e210
fordgoat <adaadaajapan@gmail.com>
2025-03-23 06:00:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ async def reset_menfess():
 
 scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
 scheduler.add_job(reset_menfess, trigger="cron", hour=1, minute=0)
-scheduler.start()
+await scheduler.start()
+while True
+await asyncio.sleep(1)
 
 Bot().run()
