b18c9a15fd82e59b9e65af80a0a976d09a776b4a
fordgoat <adaadaajapan@gmail.com>
2025-03-23 05:29:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from bot import Bot, data, Database
+import asyncio
 
 async def reset_menfess():
     db = Database(data[0])
@@ -7,8 +8,12 @@ async def reset_menfess():
     await Bot().kirim_pesan(x=str(x))
     print('PESAN PROMOTE BERHASIL DIRESET')
 
-scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
-scheduler.add_job(reset_menfess, trigger="cron", hour=1, minute=0)
-scheduler.start()
+async def main():
+    scheduler = AsyncIOScheduler(timezone="Asia/Jakarta")
+    scheduler.add_job(reset_menfess, trigger="cron", hour=1, minute=0)
+    scheduler.start()
+    
+    bot = Bot()
+    await bot.run()
 
-Bot().run()
+asyncio.run(main())
